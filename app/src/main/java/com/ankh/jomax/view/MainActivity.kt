c963c49b8955a3f6e896e3dd40fc502a72cb07cd
BruceNyoni <bruce@ikhokha.com>
2021-07-26 12:16:48 +0200

    Added a sliding bottom sheets

--- app/src/main/java/com/ankh/jomax/view/MainActivity.kt
+++ app/src/main/java/com/ankh/jomax/view/MainActivity.kt
@@ -6,7 +6,8 @@ import android.content.pm.PackageManager
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
+import android.view.View
+import android.view.ViewGroup
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -14,19 +15,29 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ankh.jomax.R
-import com.ankh.jomax.data.PenColors
 import com.ankh.jomax.databinding.ActivityMainBinding
+import com.ankh.jomax.databinding.BottomSheetBehaviorBinding
 import com.ankh.jomax.util.DrawingColors
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
     private val permissions = Manifest.permission.WRITE_EXTERNAL_STORAGE
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomSheetBehaviorBinding: BottomSheetBehaviorBinding
+    private lateinit var mainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        binding.recyclerviewColors.apply{
+
+        bottomSheetBehaviorBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.bottom_sheet_behavior,
+            mainBinding.root as ViewGroup,
+            true
+        )
+
+        mainBinding.recyclerviewColors.apply{
             adapter = ColorsAdapter(
                 this@MainActivity,
                 DrawingColors.getColors(),
@@ -36,20 +47,43 @@ class MainActivity : AppCompatActivity() {
 
         changePenColors(DrawingColors.getColors()[0].value)
 
-        binding.faBtnScreenCapture.setOnClickListener {
+        bottomSheetBehaviorBinding.faBtnScreenCapture.setOnClickListener {
             Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show()
-            binding.viewMyCanvas.clearCanvas()
+            mainBinding.viewMyCanvas.clearCanvas()
             //binding.viewMyCanvas.screenShot("test.jpg")
         }
 
-        binding.skBarPenSize.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+        bottomSheetBehaviorBinding.skBarPenSize.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {}
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                binding.viewMyCanvas.changePenSize(seekBar!!.progress.toFloat())
+                mainBinding.viewMyCanvas.changePenSize(seekBar!!.progress.toFloat())
             }
         })
+
+        val bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottom_sheet))
+        bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback(){
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                //bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+
+        })
+
+        bottomSheetBehaviorBinding.viewBottomSheetDrag.setOnClickListener {
+            if(bottomSheetBehavior.state==BottomSheetBehavior.STATE_COLLAPSED){
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }else{
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+        }
+
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
         checkPermission()
     }
 
@@ -61,8 +95,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun changePenColors(color: String){
-        binding.viewMyCanvas.changeDrawingColor(color)
-        binding.skBarPenSize.thumb.setTint(Color.parseColor(color))
+        mainBinding.viewMyCanvas.changeDrawingColor(color)
+        bottomSheetBehaviorBinding.skBarPenSize.thumb.setTint(Color.parseColor(color))
     }
 
     private fun checkPermission(){
